6f7bff0c9c22237a63456eaf8d862c58e010ed47
traveltowns <olgacackes1010@gmail.com>
2024-11-27 21:24:03 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -16,7 +16,7 @@ window.addEventListener('scroll', () => {
     const scrollPosition = window.scrollY;
 
     // Устанавливаем прозрачность стрелки в зависимости от положения прокрутки
-    if (scrollPosition > 50) { // Измените 50 на нужное значение для триггера
+    if (scrollPosition > 50) { 
         arrow.style.opacity = '0';
     } else {
         arrow.style.opacity = '1';
@@ -42,3 +42,34 @@ scrollToTopButton.addEventListener('click', (e) => {
         behavior: 'smooth' // Плавный скролл
     });
 });
+// function openPopup(imageSrc1, imageSrc2, description) {
+//     document.getElementById('popup-image-1').src = imageSrc1;
+//     document.getElementById('popup-image-2').src = imageSrc2;
+//     document.getElementById('popup-description').innerText = description;
+//     document.getElementById('popup').style.display = 'flex';
+// }
+
+// function closePopup() {
+//     document.getElementById('popup').style.display = 'none';
+// }
+function openPopup(imageSrc1, imageSrc2, description) {
+    document.getElementById('popup-image-1').src = imageSrc1;
+    document.getElementById('popup-image-2').src = imageSrc2;
+    document.getElementById('popup-description').innerText = description;
+    
+    const popup = document.getElementById('popup');
+    popup.style.display = 'flex'; // Показываем поп-ап
+    setTimeout(() => {
+        popup.classList.add('show'); // Добавляем класс для показа поп-апа
+    }, 10); // Небольшая задержка для активации анимации
+}
+
+function closePopup() {
+    const popup = document.getElementById('popup');
+    popup.classList.remove('show'); // Убираем класс для скрытия поп-апа
+
+    // Удаляем изображения и текст после завершения анимации
+    setTimeout(() => {
+        popup.style.display = 'none';
+    }, 300); // Время должно совпадать с временем анимации
+}
